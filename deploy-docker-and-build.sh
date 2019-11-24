b212708b6ef87ef2270669e9edf6cfb2021f6ab8
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-11-23 21:51:08 -0500

    Create deploy-docker-and-build.sh

---
+++ deploy-docker-and-build.sh
@@ -0,0 +1,10 @@
+
+# run as root for pulseaudio, for X11 forwarding via docker check selinux settings or !!lastly dangeously in work environemnts other security !unsafe -> setenforce 0 
+
+xhost SI:localuser:root
+mkdir -p /home/developer
+
+docker build -t marilyn-sophiabot-mod  . --no-cache
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/marilyn-current-sophiabot:mod
+
+
