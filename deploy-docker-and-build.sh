09236f9873dcce69d22ce718e8546768d1f78a9c
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-01-08 02:35:34 -0500

    Update deploy-docker-and-build.sh

--- deploy-docker-and-build.sh
+++ deploy-docker-and-build.sh
@@ -2,9 +2,9 @@
 # run as root for pulseaudio, for X11 forwarding via docker check selinux settings or !!lastly dangeously in work environemnts other security !unsafe -> setenforce 0 
 
 xhost SI:localuser:root
-mkdir -p /home/developer
+# mkdir -p /home/developer
+# docker build -t marilyn  . --no-cache
 
-docker build -t marilyn  . --no-cache
-docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/marilyn
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/marilyn-current
 
 
